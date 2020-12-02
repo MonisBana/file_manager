4e9972a1bf0ee581aaad42fe391f1c9545f8d517
Monis Bana <monisbana97@gmail.com>
2020-12-02 16:12:09 +0530

    fixed header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,11 +2,12 @@ import React from "react";
 import classes from "./Header.module.css";
 import { connect } from "react-redux";
 import { logout } from "../../redux/actions/auth";
+import logo from "../../assets/newfang.svg";
 
 function Header(props) {
   return (
     <div className={classes.header}>
-      <img src="/image/newfang.svg" alt="newFang" width="15%" height="100%" />
+      <img src={logo} alt="newFang" width="15%" height="100%" />
       <p style={{ paddingRight: "2rem" }} onClick={() => props.logout()}>
         Logout
       </p>
