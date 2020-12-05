0f5759e91ac4b3090faca2156babe38ce940e8d3
Monis Bana <monisbana97@gmail.com>
2020-12-05 16:30:21 +0530

    Remove config

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { HashRouter, Switch } from "react-router-dom";
+import { BrowserRouter, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import Routes from "./Routes";
@@ -7,11 +7,11 @@ import Routes from "./Routes";
 function App() {
   return (
     <Provider store={store}>
-      <HashRouter>
+      <BrowserRouter>
         <Switch>
           <Routes />
         </Switch>
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
   );
 }
--- src/axios-base.js
+++ src/axios-base.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const instance = axios.create({
-  baseURL: "https://interview.skizzle.email/",
-});
+const instance = axios.create({});
 
 export default instance;
